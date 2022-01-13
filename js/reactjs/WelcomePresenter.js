e40c4c14ec4c8c619790ae295a6067a351f2243d
meruem-9 <jkotieno@kth.se>
2022-01-13 16:05:11 +0100

    update

--- js/reactjs/WelcomePresenter.js
+++ js/reactjs/WelcomePresenter.js
@@ -4,13 +4,14 @@ const WelcomePresenter = (props) => {
     const [height, settingHeight] = React.useState(null);
     const [bmi, setBmi] = React.useState({bmi:0, health:""});
     const [calories, setCalories] = React.useState(null);
-    const [timeFrame, setTimeFrame] = React.useState(null);
+    const [timeFrame, setTime] = React.useState(0);
+    const [loseGain, setLoseGain] = React.useState(-1);
+    const[kg, setKg] = React.useState(0);
 
     React.useEffect(function(){
         function obs(){settingAge(props.model.age); settingWeight(props.model.weight);
-            settingHeight(props.model.height); setBmi(props.model.bmi); 
-            setCalories(props.model.calories); setTimeFrame(props.model.time);
-            //DietSource.searchBMI({age:props.model.age, weight:props.model.weight, height:props.model.height})
+            settingHeight(props.model.height); setBmi(props.model.bmi); setKg(props.model.kgTarget);
+            setCalories(props.model.calories); setTime(props.model.time); setLoseGain(props.model.loseGain);
             }
             props.model.addObserver(obs);
             return function(){props.model.removeObserver(obs)}
@@ -23,6 +24,9 @@ const WelcomePresenter = (props) => {
     bmi = {bmi.bmi}
     health = {bmi.health}
     weeks = {timeFrame}
+    loseGain = {loseGain}
+    kgTarget = {kg}
+    setKgTarget = {x => props.model.setKgTarget(x)}
     setTimeFrame = {x => props.model.setTimeFrame(x)}
     setActivity = {x => props.model.setActivityLevel(x)}
     setGender = {x => props.model.setGender(x)}
@@ -30,42 +34,9 @@ const WelcomePresenter = (props) => {
     setHeight = {x => props.model.setHeight(parseInt(x))}
     setGoals = {x => props.model.setGoals(x)}
     setWeight = {x => props.model.setWeight(parseInt(x))}
-    /* Maybe change to this? */ calculate = {x => props.model.setTimeFrame()}
+    setLoseGain = {x => props.model.setLoseGain(x)}
     />
     </div>)
 }
 
 
-function calculateGoals(x){
-    if (x == 0)
-        return "maintain weight";
-    if (x < -4)
-        return "Extreme weight loss";
-    if (x < -2)
-        return "Weight loss";
-    if (x < 0)
-        return "Mild weight loss";
-    if(x > 4)
-        return "Extreme weight gain";
-    if(x > 2)
-        return "Weight gain";
-    if(x > 0)
-        return "Mild weight gain";
-}
-
-function calculateWeeks(x){
-    if (x == 0)
-        return 0;
-    if (x < -4)
-        return (x/(-1)).toFixed(0);
-    if (x < -2)
-        return  (x/(-0.5)).toFixed(0);
-    if (x < 0)
-        return (x/(-0.25)).toFixed(0);
-    if(x > 4)
-        return (x/(1)).toFixed(0);
-    if(x > 2)
-        return (x/(0.5)).toFixed(0);
-    if(x > 0)
-        return (x/(0.25)).toFixed(0);
-}
