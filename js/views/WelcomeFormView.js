23c4bda3cd5f1ba6f2bb5db3e3dbcb149bf0e33b
meruem-9 <jkotieno@kth.se>
2022-01-13 16:03:59 +0100

    update

--- js/views/WelcomeFormView.js
+++ js/views/WelcomeFormView.js
@@ -1,13 +1,4 @@
 function WelcomeFormView(props){
-    var loseGain = -1;
-    var kg = 0;
-    var goal = "";
-    var age = 0;
-    var weight = 0;
-    var height = 0;
-    var activity = "level_1";
-    var gender = "male";
-    var time 
     return(
         <div>
             <div>
@@ -15,26 +6,26 @@ function WelcomeFormView(props){
                 <div className = "marginleftfloat">
                     
                     <label>Age:</label>
-                    <input placeholder = "enter age... " type = "text" id = "age" onInput={(e)=>{age = e.target.value; props.setAge(e.target.value)}}/>
+                    <input placeholder = "enter age... " type = "text" id = "age" onInput={(e)=>{props.setAge(e.target.value)}}/>
                     
     
                 </div>
                 <div className = "marginleftfloat">
                     
                     <label>Weight:</label>
-                    <input placeholder = "enter weight..." type = "text" id = "weight" onInput={(e)=>{weight = e.target.value; props.setWeight(e.target.value)}}/>
+                    <input placeholder = "enter weight..." type = "text" id = "weight" onInput={(e)=>{props.setWeight(e.target.value)}}/>
                     
     
                 </div>
                 <div className = "marginleftfloat">
                     <label>Height:</label>
-                    <input placeholder = "enter height..." type = "text" id = "height" onInput={(e)=>{height = e.target.value; props.setHeight(e.target.value)}}/>
+                    <input placeholder = "enter height..." type = "text" id = "height" onInput={(e)=>{props.setHeight(e.target.value)}}/>
                 </div>
     
     
                 <div className = "marginleftfloat">
                     <label htmlFor = "Activity levels"> Choose activity level:</label>
-                    <select id = "Activity levels" onChange = {e => {activity = e.target.value; props.setActivity(e.target.value)}}>
+                    <select id = "Activity levels" onChange = {e => {props.setActivity(e.target.value)}}>
                     <option value = "level_1">Little to no exercise</option>
                     <option value = "level_2">Exercise 1-3 times/week</option>
                     <option value = "level_3">Exercise 4-5 times/week</option>
@@ -47,7 +38,8 @@ function WelcomeFormView(props){
     
                 <div>
                     <label>What is your goal</label>
-                    <select id = "goal3" onChange = {e => {loseGain = parseInt(e.target.value);}}>
+                    <select id = "goal3" onChange = {e => {props.setLoseGain(parseInt(e.target.value)); props.setGoals(calculateGoals(parseInt(e.target.value) * props.kgTarget));
+                     props.setTimeFrame(calculateWeeks(parseInt(e.target.value) * props.kgTarget));}}>
                     <option value = "-1">
                         Lose weight
                     </option>
@@ -59,15 +51,15 @@ function WelcomeFormView(props){
                 
     
                 <div>
-                    <input placeholder = "Enter kg to lose/gain..." type = "Integer" onInput= {e => {kg = e.target.value; props.setGoals(calculateGoals(parseInt(e.target.value) * loseGain));
-                     props.setTimeFrame(calculateWeeks(parseInt(e.target.value) * loseGain));}}>
+                    <input placeholder = "Enter kg to lose/gain..." type = "Integer" onInput= {e => {props.setKgTarget(e.target.value); props.setGoals(calculateGoals(parseInt(e.target.value) * props.loseGain));
+                     props.setTimeFrame(calculateWeeks(parseInt(e.target.value) * props.loseGain));}}>
                     </input>
                 </div>
     
     
                 <div className = "marginleftfloat">
                     <label htmlFor = "Gender">Gender:</label>
-                    <select id = "Gender" onChange = {e => {gender = e.target.value; props.setGender(e.target.value)}}>
+                    <select id = "Gender" onChange = {e => {props.setGender(e.target.value)}}>
                     <option value = "male">Male</option>
                     <option value = "female">Female</option>
                     </select>
@@ -87,8 +79,6 @@ function WelcomeFormView(props){
     
     );
     }
-    //Might be used
-    //<span><button onClick = { e => props.calculate(age, weight, height, activity, gender, goal = calculateGoals(kg * loseGain), time = calculateWeeks(kg * loseGain))}></button></span>
     
     
     
@@ -126,16 +116,3 @@ function WelcomeFormView(props){
             return (x/(0.25)).toFixed(0);
     }
     
-    
-    /* <div className = "marginleftfloat">
-                    <label htmlFor = "Goals"> Goals:</label>
-                    <select id = "Goals" onChange = {e => {props.setGoals(e.target.value)}}>
-                    <option value = "maintain weight"> maintain weight</option>
-                    <option value = "Mild weight loss">Mild weight loss</option>
-                    <option value = "Weight loss">Weight loss</option>
-                    <option value = "Extreme weight loss">Significant weight loss</option>
-                    <option value = "Mild weight gain">Mild weight gain</option>
-                    <option value = "Weight gain">Weight gain</option>
-                    <option value = "Extreme weight gain">Significant weight gain</option>
-                    </select>
-                </div>*/
