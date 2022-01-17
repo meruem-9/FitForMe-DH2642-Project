20ee4c4e103921d8f4212cfb54ed0c48ad0a23a3
Christian <cmadjar@kth.se>
2022-01-17 14:16:38 +0100

    LatestVer17

--- js/views/PromiseNoData.js
+++
@@ -1,9 +0,0 @@
-function promiseNoData(promise, data, error){
-    if(!promise && !data && !error)
-        return <span>0</span>;
-    if(promise && !data && !error)
-        return (<img src = "http://www.csc.kth.se/~cristi/loading.gif" ></img>)
-    if(promise && !data && error)
-        return (new Error(" why error why "));
-    return false;
-}
